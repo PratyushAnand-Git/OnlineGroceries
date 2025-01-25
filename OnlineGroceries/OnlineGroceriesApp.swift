c7c442194de43969ab8d71f32e10fe93bb12079f
PratyushAnand-Git <pratyushanan@gmail.com>
2025-01-25 15:39:18 +0530

    Initial Commit

---
+++ OnlineGroceries/OnlineGroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesApp.swift
+//  OnlineGroceries
+//
+//  Created by student on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
