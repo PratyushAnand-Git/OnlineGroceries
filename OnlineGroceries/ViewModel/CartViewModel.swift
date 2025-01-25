227adc782af92f0b07b4cdb117c9afdf7988fc7e
PratyushAnand-Git <pratyushanan@gmail.com>
2025-01-25 16:23:03 +0530

    Update

---
+++ OnlineGroceries/ViewModel/CartViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CartViewModel.swift
+//  FoodDelivery
+//
+//  Created by yagnik on 19/01/25.
+//
+
+import SwiftUI
+
+struct CartViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartViewModel()
+}
---
+++ View/Login/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  OnlineGroceries
+//
+//  Created by student on 25/01/25.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
